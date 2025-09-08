f5d7b08a9a37b422cad3311d6db59be74e041618
Matthias REY <matthias.rey@epita.fr>
2025-09-08 15:33:18 +0200

    fix port

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 import joblib
+import uvicorn as unicorn
 
 app = FastAPI()
-
-
-
 
 @app.post("/predict/")
 async def predict(size: int, nb_rooms: int, garden: int):
     model = joblib.load(filename="regression.joblib")
     y_pred = model.predict([[size, nb_rooms, garden]])
     return {"y_pred": y_pred[0]}
+
+unicorn.run(app, host="0.0.0.0", port=8077)
